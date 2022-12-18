694f47bdba13fe395552c28286d5a88529d4c782
tiandt <hstdt@163.com>
2022-12-18 18:22:53 +0800

    Add SwiftUILayouts

--- Package.swift
+++ Package.swift
@@ -6,15 +6,17 @@ import PackageDescription
 let package = Package(name: "ASCollectionView",
                       platforms: [.iOS(.v11)],
                       products: [// Products define the executables and libraries produced by a package, and make them visible to other packages.
-                      	.library(name: "ASCollectionView",
-                      	         targets: ["ASCollectionView"]),
-                        .library(name: "ASCollectionViewDynamic", type: .dynamic, targets: ["ASCollectionView"]),
-					  ],
+                        .library(name: "ASCollectionView",
+                                 targets: ["ASCollectionView", "SwiftUILayouts"]),
+                        .library(name: "ASCollectionViewDynamic",
+                                 type: .dynamic, targets: ["ASCollectionView"]),
+                      ],
                       dependencies: [
 						.package(url: "https://github.com/ra1028/DifferenceKit", .upToNextMajor(from: Version(1, 1, 5)))
                       ],
                       targets: [
                       	.target(name: "ASCollectionView",
                       	        dependencies: ["DifferenceKit"]),
+                        .target(name: "SwiftUILayouts"),
 					  ]
 )
